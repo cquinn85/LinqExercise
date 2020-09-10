2b5534ece4dc06dbcbda73f4edf9ccea4744d3fb
cquinn85 <cequinn300@gmail.com>
2020-09-10 11:41:40 -0500

    Game List by length of name

---
+++ GameList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameList = new List<string> { "Super Mario Brothers", "Destiny 2", "Fall Out Guys", "Contra", "Tomb Raider: Rise of the Tomb Raider" };
+            var nameLength = gameList.OrderBy(game => game.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
